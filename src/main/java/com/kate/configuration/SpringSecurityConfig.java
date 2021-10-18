5e651c907a19afddbdc806bc1e71a13e2bc13f77
Oleg Zaidullin <cornknight@gmail.com>
2021-10-18 11:47:53 +0600

    initial commit

---
+++ src/main/java/com/kate/configuration/SpringSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.kate.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.csrf()
+		    .disable()
+		    .authorizeRequests()
+		    .antMatchers("/","/hello/**", "/css/**", "/img/**").permitAll()
+		    .anyRequest()
+		    .authenticated();
+		    //.and()
+		    //.formLogin()
+		    //.loginPage("/login")
+		    //.permitAll()
+		   // .and()
+		    //.logout()
+		    //.permitAll();
+	}
+}
---
+++ src/test/java/com/kate/DsApplicationTests.java
@@ -0,0 +1,12 @@
+package com.kate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class DsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
