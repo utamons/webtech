3bc906c471622c19e700ddbae40a55a5cc10d268
Oleg Zaidullin <cornknight@gmail.com>
2021-10-19 12:39:15 +0600

    added date

--- src/main/java/com/kate/controller/HelloController.java
+++ src/main/java/com/kate/controller/HelloController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Date;
+
 @Controller
 @RequestMapping("/")
 public class HelloController {
@@ -12,6 +14,7 @@ public class HelloController {
 	@GetMapping("/")
 	public String getIndex(Model model) {
 		model.addAttribute("name", "Kate");
+		model.addAttribute("dt", new Date());
 		return "index";
 	}
 }
