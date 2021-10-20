706918ecad2160c1a97217bdb368549cb378aeb4
Oleg Zaidullin <cornknight@gmail.com>
2021-10-20 11:14:13 +0600

    added table

--- src/main/java/com/kate/controller/HelloController.java
+++ src/main/java/com/kate/controller/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Date;
+import java.util.Random;
 
 @Controller
 @RequestMapping("/")
@@ -13,8 +14,15 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String getIndex(Model model) {
+		Random random = new Random();
+		int[][] x = new int[3][3];
+		for (int i=0; i<3;++i)
+			for (int j=0; j<3;++j)
+				x[i][j] = random.nextInt(10);
+
 		model.addAttribute("name", "Kate");
 		model.addAttribute("dt", new Date());
+		model.addAttribute("x",x);
 		return "index";
 	}
 }
